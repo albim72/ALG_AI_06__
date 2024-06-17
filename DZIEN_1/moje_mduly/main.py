f410f7e26654a49e8ace300db9d99435567d8c95
albim72 <marcin.albiniak72@gmail.com>
2024-06-17 11:18:00 +0200

    Update main.py

--- DZIEN_1/moje_mduly/main.py
+++ DZIEN_1/moje_mduly/main.py
@@ -2,6 +2,13 @@
 # import dane as dn
 from dane import nrfilii as nf
 from dane import book as bk
+from moje_funkcje.fkolekcje import czytaj_liste,czytaj_slownik
 
 print(nf)
 print(bk)
+
+print(f"użycie funkcji z modułu fkolekcje.py")
+print(f"lista:")
+czytaj_liste(nf)
+print(f"słownik:")
+czytaj_slownik(bk)
